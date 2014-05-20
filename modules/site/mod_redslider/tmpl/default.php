d91c5612aeeb4fb96ce32d6a928893eea55531b3
anhredweb <anh@redweb.dk>
2014-05-20 15:11:06 +0700

    RSLIDER-24 #comment Create Phing files

--- modules/site/mod_redslider/tmpl/default.php
+++ modules/site/mod_redslider/tmpl/default.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @package     RedITEM.Frontend
- * @subpackage  mod_reditem_relateditems
+ * @package     RedSLIDER.Frontend
+ * @subpackage  mod_redslider
  *
- * @copyright   Copyright (C) 2005 - 2013 redCOMPONENT.com. All rights reserved.
+ * @copyright   Copyright (C) 2014 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
