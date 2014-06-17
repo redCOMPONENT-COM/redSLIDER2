e44119ac031b9d76a3259681633ab067dfac6bfb
Kyle Hoang <cuong@redweb.dk>
2014-06-17 18:16:48 +0700

    RSLIDER-36 #comment Create a new search tool that filters slides by gallery

--- component/admin/models/slides.php
+++ component/admin/models/slides.php
@@ -155,8 +155,8 @@ class RedsliderModelSlides extends RModelList
 		$published = $this->getUserStateFromRequest($this->context . '.filter.published', 'filter_published', '');
 		$this->setState('filter.published', $published);
 
-		$published = $this->getUserStateFromRequest($this->context . '.filter.published', 'filter_published', '');
-		$this->setState('filter.gallery_id', $published);
+		$gallery_id = $this->getUserStateFromRequest($this->context . '.filter.gallery_id', 'filter_gallery_id', '');
+		$this->setState('filter.gallery_id', $gallery_id);
 
 		$value = $app->getUserStateFromRequest('global.list.limit', $this->paginationPrefix . 'limit', $app->getCfg('list_limit'), 'uint');
 		$limit = $value;
