c2b48b53aa666fc7e7614ae6ead036163de98bf9
Kyle Hoang <cuong@redweb.dk>
2014-10-14 11:27:59 +0700

    RSLIDER-112 #comment Remove gallery and template: should delete all related slides also

--- component/admin/tables/template.php
+++ component/admin/tables/template.php
@@ -40,4 +40,31 @@ class RedsliderTableTemplate extends RTable
 	 * @var  string
 	 */
 	protected $_tableFieldState = 'published';
+
+	/**
+	 * Deletes this row in database (or if provided, the row of key $pk)
+	 *
+	 * @param   mixed  $pk  An optional primary key value to delete.  If not set the instance property value is used.
+	 *
+	 * @return  boolean  True on success.
+	 */
+	public function delete($pk = null)
+	{
+		$db            = RFactory::getDbo();
+		$user          = RFactory::getUser();
+		$query         = $db->getQuery(true);
+		$templateModel = RModel::getAdminInstance('Template', array('ignore_request' => true), 'com_redslider');
+
+		if ($pk)
+		{
+			// Remove related slides
+			$query->clear()
+				->delete($db->qn('#__redslider_slides'))
+				->where($db->qn('template_id') . '=' . (int) $pk);
+			$db->setQuery($query);
+			$db->execute();
+		}
+
+		return parent::delete($pk);
+	}
 }
