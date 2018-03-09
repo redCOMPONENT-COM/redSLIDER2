17e45b0d9c6a22d0e7e96bce9d87e5b90d031a21
Thong Tran Quang <thong@redweb.dk>
2018-03-09 16:23:44 +0700

    CL-46 [Fix] Preview JS error when edit slide. (#184)
    * CL-46 [Fix] Preview JS error when edit slide.
    
    * CL-46 [Fix] Preview JS error when edit slide.

--- extensions/components/com_redslider/admin/views/slide/tmpl/edit.php
+++ extensions/components/com_redslider/admin/views/slide/tmpl/edit.php
@@ -11,7 +11,7 @@ defined('_JEXEC') or die;
 JHtml::_('rjquery.chosen', 'select');
 
 $isNew = true;
-$user = JFactory::getUser();
+$user  = JFactory::getUser();
 JPluginHelper::importPlugin('redslider_sections');
 $dispatcher = RFactory::getDispatcher();
 
@@ -21,150 +21,146 @@ if ($this->item->id)
 }
 ?>
 <script type="text/javascript">
-	jQuery(document).ready(function()
-	{
-		// Disable click function on btn-group
-		jQuery(".btn-group").each(function(index){
-			if (jQuery(this).hasClass('disabled'))
-			{
-				jQuery(this).find("label").off('click');
-			}
-		});
-	});
+    jQuery(document).ready(function () {
+        // Disable click function on btn-group
+        jQuery(".btn-group").each(function (index) {
+            if (jQuery(this).hasClass("disabled")) {
+                jQuery(this).find("label").off("click");
+            }
+        });
+    });
 </script>
 <form enctype="multipart/form-data"
-	action="index.php?option=com_redslider&task=slide.edit&id=<?php echo $this->item->id; ?>"
-	method="post" name="adminForm" class="form-validate form-horizontal" id="adminForm">
+      action="index.php?option=com_redslider&task=slide.edit&id=<?php echo $this->item->id; ?>"
+      method="post" name="adminForm" class="form-validate form-horizontal" id="adminForm">
 
-	<div class="row-fluid">
-		<div class="span4">
-		<div class="control-group">
-			<div class="control-label">
-				<?php echo $this->form->getLabel('section'); ?>
-			</div>
-			<div class="controls">
-				<?php echo $this->form->getInput('section'); ?>
-			</div>
-		</div>
-		<div class="control-group">
-			<div class="control-label">
-				<?php echo $this->form->getLabel('gallery_id'); ?>
-			</div>
-			<div class="controls">
-				<?php echo $this->form->getInput('gallery_id'); ?>
-			</div>
-		</div>
-		<div class="control-group">
-			<div class="control-label">
-				<?php echo $this->form->getLabel('template_id'); ?>
-			</div>
-			<div class="controls">
-				<?php echo $this->form->getInput('template_id'); ?>
-			</div>
-		</div>
-		<div class="control-group">
-			<div class="control-label">
-				<?php echo $this->form->getLabel('title'); ?>
-			</div>
-			<div class="controls">
-				<?php echo $this->form->getInput('title'); ?>
-			</div>
-		</div>
-		<div class="control-group">
-			<div class="control-label">
-				<?php echo $this->form->getLabel('alias'); ?>
-			</div>
-			<div class="controls">
-				<?php echo $this->form->getInput('alias'); ?>
-			</div>
-		</div>
-		</div>
-		<div class="span4">
-			<div class="control-group">
-				<div class="control-label">
+    <div class="row-fluid">
+        <div class="span4">
+            <div class="control-group">
+                <div class="control-label">
+					<?php echo $this->form->getLabel('section'); ?>
+                </div>
+                <div class="controls">
+					<?php echo $this->form->getInput('section'); ?>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label">
+					<?php echo $this->form->getLabel('gallery_id'); ?>
+                </div>
+                <div class="controls">
+					<?php echo $this->form->getInput('gallery_id'); ?>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label">
+					<?php echo $this->form->getLabel('template_id'); ?>
+                </div>
+                <div class="controls">
+					<?php echo $this->form->getInput('template_id'); ?>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label">
+					<?php echo $this->form->getLabel('title'); ?>
+                </div>
+                <div class="controls">
+					<?php echo $this->form->getInput('title'); ?>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label">
+					<?php echo $this->form->getLabel('alias'); ?>
+                </div>
+                <div class="controls">
+					<?php echo $this->form->getInput('alias'); ?>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label">
+					<?php echo $this->form->getLabel('created_date'); ?>
+                </div>
+                <div class="controls">
+					<?php echo $this->form->getInput('created_date'); ?>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label">
+					<?php echo $this->form->getLabel('created_by'); ?>
+                </div>
+                <div class="controls">
+					<?php echo $this->form->getInput('created_by'); ?>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label">
+					<?php echo $this->form->getLabel('modified_date'); ?>
+                </div>
+                <div class="controls">
+					<?php echo $this->form->getInput('modified_date'); ?>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label">
+					<?php echo $this->form->getLabel('modified_by'); ?>
+                </div>
+                <div class="controls">
+					<?php echo $this->form->getInput('modified_by'); ?>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label">
+					<?php echo $this->form->getLabel('publish_up'); ?>
+                </div>
+                <div class="controls">
+					<?php echo $this->form->getInput('publish_up'); ?>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label">
+					<?php echo $this->form->getLabel('publish_down'); ?>
+                </div>
+                <div class="controls">
+					<?php echo $this->form->getInput('publish_down'); ?>
+                </div>
+            </div>
+			<?php echo $this->form->getInput('checked_out'); ?>
+			<?php echo $this->form->getInput('checked_out_time'); ?>
+        </div>
+        <div class="span6">
+            <div class="control-group">
+                <div class="control-label">
 					<?php echo $this->form->getLabel('published'); ?>
-				</div>
-				<div class="controls">
+                </div>
+                <div class="controls">
 					<?php echo $this->form->getInput('published'); ?>
-				</div>
-			</div>
+                </div>
+            </div>
 
-			<div class="control-group">
-				<div class="control-label">
+            <div class="control-group">
+                <div class="control-label">
 					<?php echo $this->form->getLabel('language'); ?>
-				</div>
-				<div class="controls">
+                </div>
+                <div class="controls">
 					<?php echo $this->form->getInput('language'); ?>
-				</div>
-			</div>
+                </div>
+            </div>
 
 			<?php // Load template dynamically from plugin ?>
 			<?php if ($user->authorise('core.create', 'com_redslider') && $user->authorise('core.edit', 'com_redslider') && $user->authorise('core.edit.state', 'com_redslider')): ?>
 				<?php if ($this->sectionId): ?>
 					<?php $sectionTemplates = $dispatcher->trigger('onSlidePrepareTemplate', array($this, $this->sectionId)); ?>
-					<?php if (count($sectionTemplates)):?>
+					<?php if (count($sectionTemplates)): ?>
 						<?php foreach ($sectionTemplates as $template): ?>
 							<?php echo $template ?>
 						<?php endforeach; ?>
 					<?php endif; ?>
 				<?php endif; ?>
-			<?php endif;?>
-		</div>
-		<div class="span4">
-			<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('created_date'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('created_date'); ?>
-				</div>
-			</div>
-			<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('created_by'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('created_by'); ?>
-				</div>
-			</div>
-			<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('modified_date'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('modified_date'); ?>
-				</div>
-			</div>
-			<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('modified_by'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('modified_by'); ?>
-				</div>
-			</div>
-			<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('publish_up'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('publish_up'); ?>
-				</div>
-			</div>
-			<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('publish_down'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('publish_down'); ?>
-				</div>
-			</div>
-			<?php echo $this->form->getInput('checked_out'); ?>
-			<?php echo $this->form->getInput('checked_out_time'); ?>
-		</div>
-	</div>
+			<?php endif; ?>
+        </div>
+    </div>
 
 	<?php echo $this->form->getInput('id'); ?>
-	<input type="hidden" name="task" value="" />
+    <input type="hidden" name="task" value=""/>
 	<?php echo JHtml::_('form.token'); ?>
 </form>
